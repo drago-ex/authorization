a7ce27e6d481d00ffd18fa960af6f246db148526
Zdeněk Papučík <zdenek.papucik@gmail.com>
2020-06-02 10:19:13 +0200

    Update and rename Roles.php to RolesEntity.php

---
+++ src/Drago/Authorization/src/entity/RolesEntity.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drago\Authorization\Entity;
+
+/**
+ * @property int $roleId
+ * @property string $name
+ * @property int $parent
+ */
+class RolesEntity extends \Drago\Database\Entity
+{
+	const TABLE = 'roles';
+	const ROLE_ID = 'roleId';
+	const NAME = 'name';
+	const PARENT = 'parent';
+
+	/** @var int */
+	public $roleId;
+
+	/** @var string */
+	public $name;
+
+	/** @var int */
+	public $parent;
+
+
+	public function getRoleId(): ?int
+	{
+		return $this->roleId;
+	}
+
+
+	public function setRoleId(int $var)
+	{
+		$this['roleId'] = $var;
+	}
+
+
+	public function getName(): string
+	{
+		return $this->name;
+	}
+
+
+	public function setName(string $var)
+	{
+		$this['name'] = $var;
+	}
+
+
+	public function getParent(): int
+	{
+		return $this->parent;
+	}
+
+
+	public function setParent(int $var)
+	{
+		$this['parent'] = $var;
+	}
+}
