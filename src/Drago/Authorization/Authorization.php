befa5007111fc4d862fc1321d87dd01215426d0c
Zdeněk Papučík <zdenek.papucik@gmail.com>
2021-05-05 08:06:25 +0200

    Update Authorization.php

--- src/Drago/Authorization/Authorization.php
+++ src/Drago/Authorization/Authorization.php
@@ -22,19 +22,15 @@ trait Authorization
 	public function injectAuthorization(Presenter $presenter, User $user): void
 	{
 		$presenter->onStartup[] = function () use ($presenter, $user) {
-			try {
-				$signal = $presenter->getSignal();
-				if ((!empty($signal[0])) && isset($signal[1])) {
-					if (!$user->isAllowed($presenter->getName(), $signal[0])) {
-						$presenter->error('Forbidden', 403);
-					}
-				} else {
-					if (!$user->isAllowed($presenter->getName(), $signal[1] ?? $presenter->getAction())) {
-						$presenter->error('Forbidden', 403);
-					}
+			$signal = $presenter->getSignal();
+			if ((!empty($signal[0])) && isset($signal[1])) {
+				if (!$user->isAllowed($presenter->getName(), $signal[0])) {
+					$presenter->error('Forbidden', 403);
 				}
-			} catch (InvalidStateException $e) {
-				// Not implemented.
+			} else {
+				if (!$user->isAllowed($presenter->getName(), $signal[1] ?? $presenter->getAction())) {
+					$presenter->error('Forbidden', 403);
+				}
 			}
 		};
 	}
