2512455607631bb042f391fccbdae4d4616c93d8
Zdeněk Papučík <zdenek.papucik@gmail.com>
2021-05-07 10:47:52 +0200

    Update Component.php

--- src/Drago/Authorization/Control/Component.php
+++ src/Drago/Authorization/Control/Component.php
@@ -12,6 +12,8 @@ namespace Drago\Authorization\Control;
 
 use Drago\Application\UI;
 use Drago\Authorization\FileNotFoundException;
+use Nette\Application\UI\Form;
+use Nette\Bridges\ApplicationLatte\Template;
 use Nette\Localization\Translator;
 use Nette\SmartObject;
 use stdClass;
@@ -30,6 +32,7 @@ abstract class Component extends UI\ExtraControl
 	public ?string $templateAdd = null;
 	public ?string $templateRecords = null;
 	public int $deleteId = 0;
+
 	protected string $snippetError = 'error';
 	protected string $snippetMessage = 'message';
 	protected string $snippetPermissions = 'permissions';
@@ -41,6 +44,14 @@ abstract class Component extends UI\ExtraControl
 	public function redrawPresenter(string $snippet = null, bool $redraw = true): void
 	{
 		$this->presenter->redrawControl($snippet, $redraw);
+	}
+
+
+	public function multipleRedrawPresenter(array $snippets): void
+	{
+		foreach ($snippets as $snippet) {
+			$this->redrawPresenter($snippet);
+		}
 	}
 
 
@@ -76,5 +87,43 @@ abstract class Component extends UI\ExtraControl
 	public function setTranslator(Translator $translator): Translator
 	{
 		return $this->translator = $translator;
+	}
+
+
+	public function setRenderControl(string $templateFile, ?Form $form = null, ?array $items = []): void
+	{
+		if ($this->template instanceof Template) {
+			$template = $this->template;
+
+			if ($form instanceof Form) {
+				$template->form = $form;
+			}
+
+			if (is_array($items)) {
+				foreach ($items as $key => $item) {
+					$template->$key = $item;
+				}
+			}
+
+			if ($this->translator instanceof Translator) {
+				$template->setTranslator($this->translator);
+			}
+
+			$template->setFile($templateFile);
+			$template->render();
+
+		} else {
+			throw new InvalidStateException('Incorrect instance type.');
+		}
+	}
+
+
+	public function factory(): Form
+	{
+		$form = new Form;
+		if ($this->translator instanceof Translator) {
+			$form->setTranslator($this->translator);
+		}
+		return $form;
 	}
 }
