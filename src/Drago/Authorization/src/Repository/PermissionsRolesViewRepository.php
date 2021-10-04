33b403d6058d3593f8dbe24cbd5b066febe80f61
Zdeněk Papučík <zdenek.papucik@gmail.com>
2021-10-04 06:16:14 +0200

    Update PermissionsRolesViewRepository.php

--- src/Drago/Authorization/src/Repository/PermissionsRolesViewRepository.php
+++ src/Drago/Authorization/src/Repository/PermissionsRolesViewRepository.php
@@ -9,14 +9,11 @@ declare(strict_types=1);
 
 namespace Drago\Authorization\Repository;
 
+use Drago\Attr\Table;
 use Drago\Authorization\Entity\PermissionsRolesViewEntity;
 use Drago\Database\Connect;
 use Drago\Database\Repository;
 
 
-class PermissionsRolesViewRepository extends Connect
-{
-	use Repository;
-
-	public string $table = PermissionsRolesViewEntity::TABLE;
-}
+#[Table(PermissionsRolesViewEntity::TABLE)]
+class PermissionsRolesViewRepository extends Connect {}
