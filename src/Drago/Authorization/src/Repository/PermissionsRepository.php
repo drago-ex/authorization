e9933df48c67b8416cd9efa38dd05f1224a3542a
Zdeněk Papučík <zdenek.papucik@gmail.com>
2021-10-04 06:15:22 +0200

    Update PermissionsRepository.php

--- src/Drago/Authorization/src/Repository/PermissionsRepository.php
+++ src/Drago/Authorization/src/Repository/PermissionsRepository.php
@@ -10,26 +10,20 @@ declare(strict_types=1);
 namespace Drago\Authorization\Repository;
 
 use Dibi\Exception;
+use Drago\Attr\Table;
 use Drago\Authorization\Entity\PermissionsEntity;
 use Drago\Database\Connect;
 use Drago\Database\Repository;
 
 
+#[Table(PermissionsEntity::TABLE, PermissionsEntity::PRIMARY)]
 class PermissionsRepository extends Connect
 {
-	use Repository;
-
-	public string $table = PermissionsEntity::TABLE;
-	public string $primary = PermissionsEntity::PRIMARY;
-
-
 	/**
 	 * @throws Exception
 	 */
 	public function getRecord(int $id): array|PermissionsEntity|null
 	{
-		return $this->get($id)->execute()
-			->setRowClass(PermissionsEntity::class)
-			->fetch();
+		return $this->get($id)->fetch();
 	}
 }
