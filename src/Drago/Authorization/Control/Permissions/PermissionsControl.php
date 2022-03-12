6106422943cfcf38e01cf895acbd9f22c8dbf59a
Zdeněk Papučík <zdenek.papucik@gmail.com>
2022-03-12 07:40:20 +0100

    Update PermissionsControl.php

--- src/Drago/Authorization/Control/Permissions/PermissionsControl.php
+++ src/Drago/Authorization/Control/Permissions/PermissionsControl.php
@@ -96,7 +96,7 @@ class PermissionsControl extends Component implements Base
 			}
 
 			if ($this->isAjax()) {
-				$this->presenter->payload->{$this->snippetFactory} = $this->snippetFactory;
+				$this->getPresenter->payload->{$this->snippetFactory} = $this->snippetFactory;
 				$this->redrawPresenter($this->snippetFactory);
 			}
 		}
@@ -233,7 +233,7 @@ class PermissionsControl extends Component implements Base
     public function handleClickOpen()
     {
 		if ($this->isAjax()) {
-			$this->presenter->payload->{$this->snippetFactory} = $this->snippetFactory;
+			$this->getPresenter->payload->{$this->snippetFactory} = $this->snippetFactory;
 			$this->redrawPresenter($this->snippetFactory);
 		}
     }
