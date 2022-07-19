bd1445f54f1ff6fa4b6aac49d8d16adb3d06ccec
Zdeněk Papučík <zdenek.papucik@gmail.com>
2022-07-19 07:01:05 +0200

    Update ResourcesControl.php

--- src/Drago/Authorization/Control/Resources/ResourcesControl.php
+++ src/Drago/Authorization/Control/Resources/ResourcesControl.php
@@ -223,7 +223,6 @@ class ResourcesControl extends Component implements Base
 			$form->addError($message);
 			if ($this->isAjax()) {
 				$this->getPresenter()->redrawControl($this->snippetFactory);
-				$this->redrawControl($this->snippetError);
 			}
 		}
 	}
