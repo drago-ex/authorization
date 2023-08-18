1c4f7b27afc8aff204358d3f36996a9f2ce6a3c3
accgit <zdenek.papucik@gmail.com>
2023-08-18 12:13:22 +0200

    dev

--- src/Drago/Authorization/Control/Access/UsersRolesViewRepository.php
+++ src/Drago/Authorization/Control/Access/UsersRolesViewRepository.php
@@ -37,7 +37,7 @@ class UsersRolesViewRepository
 	public function getAllUsers(): Fluent
 	{
 		return $this->db
-			->select('user_id, username, group_concat(role separator ", ") role')
+			->select('user_id, username, LISTAGG(role, ", ") role')
 			->from($this->getTable())->groupBy('user_id, username')
 			->having('SUM(CASE WHEN role = ? THEN 1 ELSE 0 END) = ?', Conf::ROLE_ADMIN, 0)
 			->orderBy(UsersRolesViewEntity::USER_ID, 'asc');
