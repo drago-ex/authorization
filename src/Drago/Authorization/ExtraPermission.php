ce2c2ca557f5e5b68558953ca852c9d40bb7466e
accgit <zdenek.papucik@gmail.com>
2023-09-15 07:21:47 +0200

    Fix

--- src/Drago/Authorization/ExtraPermission.php
+++ src/Drago/Authorization/ExtraPermission.php
@@ -47,10 +47,10 @@ class ExtraPermission
 		try {
 			if (!$this->cache->load(Conf::Cache)) {
 				$rolesQueue = $this->rolesRepository->getAllRoles();
-				while(count($rolesQueue) > 0) {
+				while (count($rolesQueue) > 0) {
 					$role = array_shift($rolesQueue);
 					$parent = $this->rolesRepository->findByParent($role->parent);
-					$parent === null || in_array($parent->name, $acl->getRoles())
+					$parent === null || in_array($parent->name, $acl->getRoles(), true)
 						? $acl->addRole($role->name, $parent->name ?? null)
 						: array_push($rolesQueue, $role);
 				}
