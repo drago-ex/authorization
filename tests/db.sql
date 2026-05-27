65c2082b3eeb9bc71f4bd9a1496ba4ad54f2b85c
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-05-27 12:03:36 +0200

    Update db.sql

--- tests/db.sql
+++ tests/db.sql
@@ -1,122 +1,230 @@
 --
 --  Simple dynamic access control list management.
 -- -----------------------------------------------
-
-SET NAMES utf8;
+SET NAMES utf8mb4;
 SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `permissions`;
-CREATE TABLE `permissions` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `role_id` int(11) unsigned NOT NULL,
-  `resource_id` int(11) unsigned NOT NULL,
-  `privilege_id` int(11) unsigned NOT NULL,
-  `allowed` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `role_id_resource_id_privilege_id` (`role_id`,`resource_id`,`privilege_id`),
-  KEY `resource` (`resource_id`),
-  KEY `role` (`role_id`),
-  KEY `privilege` (`privilege_id`),
-  CONSTRAINT `permissions_ibfk_1` FOREIGN KEY (`resource_id`) REFERENCES `resources` (`id`),
-  CONSTRAINT `permissions_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
-  CONSTRAINT `permissions_ibfk_3` FOREIGN KEY (`privilege_id`) REFERENCES `privileges` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SET FOREIGN_KEY_CHECKS = 0;
 
-INSERT INTO `permissions` (`id`, `role_id`, `resource_id`, `privilege_id`, `allowed`) VALUES
-(1,	1,	1,	1,	1),
-(2,	1,	2,	2,	1),
-(3,	1,	3,	1,	1),
-(4,	3,	2,	1,	1);
+DROP VIEW IF EXISTS permissions_roles_view;
+DROP VIEW IF EXISTS permissions_view;
+DROP VIEW IF EXISTS users_roles_view;
 
-DROP VIEW IF EXISTS `permissions_roles_view`;
-CREATE TABLE `permissions_roles_view` (`id` int(11) unsigned, `name` varchar(40), `parent` int(11));
+DROP TABLE IF EXISTS users_roles;
+DROP TABLE IF EXISTS permissions;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS resources;
+DROP TABLE IF EXISTS privileges;
+DROP TABLE IF EXISTS settings;
 
+SET FOREIGN_KEY_CHECKS = 1;
 
-DROP VIEW IF EXISTS `permissions_view`;
-CREATE TABLE `permissions_view` (`id` int(11) unsigned, `resource` varchar(40), `privilege` varchar(40), `role` varchar(40), `allowed` tinyint(1));
+--
+--  Database privileges
+-- --------------------
+CREATE TABLE privileges (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(40) NOT NULL,
 
+    PRIMARY KEY (id),
+    UNIQUE KEY uq_privileges_name (name)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `privileges`;
-CREATE TABLE `privileges` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(40) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO privileges (id, name) VALUES
+    (1, '*'),
+    (2, 'default');
 
-INSERT INTO `privileges` (`id`, `name`) VALUES
-(1,	'*'),
-(2,	'default');
+--
+--  Database resources
+-- -------------------
+CREATE TABLE resources (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(40) NOT NULL,
 
-DROP TABLE IF EXISTS `resources`;
-CREATE TABLE `resources` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(40) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    PRIMARY KEY (id),
+    UNIQUE KEY uq_resources_name (name)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
 
-INSERT INTO `resources` (`id`, `name`) VALUES
-(1,	'Front:Home'),
-(2,	'Backend:Admin'),
-(3,	'Backend:Sign'),
-(4,	'Backend:Permissions');
+INSERT INTO resources (id, name) VALUES
+    (1, 'Front:Home'),
+    (2, 'Backend:Admin'),
+    (3, 'Backend:Sign'),
+    (4, 'Backend:Permissions');
 
-DROP TABLE IF EXISTS `roles`;
-CREATE TABLE `roles` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(40) NOT NULL,
-  `parent` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+--  Database roles
+-- ----------------
+CREATE TABLE roles (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(40) NOT NULL,
+    parent INT UNSIGNED NOT NULL DEFAULT 0,
 
-INSERT INTO `roles` (`id`, `name`, `parent`) VALUES
-(1,	'guest',	0),
-(2,	'member',	1),
-(3,	'admin',	2);
+    PRIMARY KEY (id),
+    UNIQUE KEY uq_roles_name (name)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `settings`;
-CREATE TABLE `settings` (
-  `name` varchar(100) NOT NULL,
-  `value` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO roles (id, name, parent) VALUES
+    (1, 'guest', 0),
+    (2, 'member', 1),
+    (3, 'admin', 2);
 
+--
+--  Database permissions
+-- ----------------------
+CREATE TABLE permissions (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    role_id INT UNSIGNED NOT NULL,
+    resource_id INT UNSIGNED NOT NULL,
+    privilege_id INT UNSIGNED NOT NULL,
+    allowed TINYINT(1) NOT NULL,
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(60) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    PRIMARY KEY (id),
 
+    UNIQUE KEY uq_permissions (
+        role_id,
+        resource_id,
+        privilege_id
+    ),
 
-DROP TABLE IF EXISTS `users_roles`;
-CREATE TABLE `users_roles` (
-  `role_id` int(11) unsigned NOT NULL,
-  `user_id` int(11) unsigned NOT NULL,
-  UNIQUE KEY `role_id_user_id` (`role_id`,`user_id`),
-  KEY `user` (`user_id`),
-  KEY `role` (`role_id`),
-  CONSTRAINT `users_roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `users_roles_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    CONSTRAINT fk_permissions_role
+        FOREIGN KEY (role_id)
+            REFERENCES roles (id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
 
+    CONSTRAINT fk_permissions_resource
+        FOREIGN KEY (resource_id)
+            REFERENCES resources (id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
 
-DROP VIEW IF EXISTS `users_roles_view`;
-CREATE TABLE `users_roles_view` (`user_id` int(11) unsigned, `username` varchar(50), `role` varchar(40));
+    CONSTRAINT fk_permissions_privilege
+        FOREIGN KEY (privilege_id)
+            REFERENCES privileges (id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
 
+INSERT INTO permissions (
+    id,
+    role_id,
+    resource_id,
+    privilege_id,
+    allowed
+) VALUES
+    (1, 1, 1, 1, 1),
+    (2, 1, 2, 2, 1),
+    (3, 1, 3, 1, 1),
+    (4, 3, 2, 1, 1);
 
-DROP TABLE IF EXISTS `permissions_roles_view`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `permissions_roles_view` AS select `roles`.`id` AS `id`,`roles`.`name` AS `name`,`roles`.`parent` AS `parent` from `roles` where `roles`.`id` in (select distinct `permissions`.`role_id` from `permissions` where `roles`.`name` <> 'admin');
+--
+--  Database settings
+-- -------------------
+CREATE TABLE settings (
+    name VARCHAR(100) NOT NULL,
+    value VARCHAR(255) NOT NULL
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `permissions_view`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `permissions_view` AS select `p`.`id` AS `id`,`r`.`name` AS `resource`,`p2`.`name` AS `privilege`,`r2`.`name` AS `role`,`p`.`allowed` AS `allowed` from (((`permissions` `p` left join `resources` `r` on(`p`.`resource_id` = `r`.`id`)) left join `privileges` `p2` on(`p`.`privilege_id` = `p2`.`id`)) left join `roles` `r2` on(`p`.`role_id` = `r2`.`id`));
+--
+--  Database users
+-- ----------------
+CREATE TABLE users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(60) NOT NULL,
 
-DROP TABLE IF EXISTS `users_roles_view`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `users_roles_view` AS select `u`.`id` AS `user_id`,`u`.`username` AS `username`,`r`.`name` AS `role` from ((`users_roles` `ur` left join `roles` `r` on(`r`.`id` = `ur`.`role_id`)) left join `users` `u` on(`u`.`id` = `ur`.`user_id`));
+    PRIMARY KEY (id),
+    UNIQUE KEY uq_users_email (email)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
+
+--
+--  Database users_roles
+-- ----------------------
+CREATE TABLE users_roles (
+    user_id INT UNSIGNED NOT NULL,
+    role_id INT UNSIGNED NOT NULL,
+
+    PRIMARY KEY (user_id, role_id),
+
+    CONSTRAINT fk_users_roles_user
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+
+    CONSTRAINT fk_users_roles_role
+        FOREIGN KEY (role_id)
+            REFERENCES roles (id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
+
+--
+--  View permissions_roles_view
+-- -----------------------------
+CREATE VIEW permissions_roles_view AS
+SELECT
+    roles.id,
+    roles.name,
+    roles.parent
+FROM roles
+WHERE roles.id IN (
+    SELECT DISTINCT permissions.role_id
+    FROM permissions
+    WHERE roles.name <> 'admin'
+);
+
+--
+--  View permissions_view
+-- -----------------------
+CREATE VIEW permissions_view AS
+SELECT
+    p.id,
+    r.name AS resource,
+    pr.name AS privilege,
+    ro.name AS role,
+    p.allowed
+FROM permissions p
+    LEFT JOIN resources r
+        ON p.resource_id = r.id
+    LEFT JOIN privileges pr
+        ON p.privilege_id = pr.id
+    LEFT JOIN roles ro
+        ON p.role_id = ro.id;
+
+--
+--  View users_roles_view
+-- -----------------------
+CREATE VIEW users_roles_view AS
+SELECT
+    u.id AS user_id,
+    u.username,
+    r.name AS role
+FROM users_roles ur
+    LEFT JOIN users u
+        ON ur.user_id = u.id
+    LEFT JOIN roles r
+        ON ur.role_id = r.id;
